4e3e26a2c140efe320c109a209579e89a0cd5920
aaronshin43 <aaronshin.exe@gmail.com>
2026-01-17 13:33:27 -0500

    Update JSON protocol

--- shared/protocol.py
+++ shared/protocol.py
@@ -1,17 +1,33 @@
-# shared/protocol.py
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, field, asdict
+import time
 import json
+from typing import Any, Dict
+
+@dataclass
+class PacketMeta:
+    category: str  # VISION, SCREEN, SYSTEM
+    timestamp: float = field(default_factory=time.time)
 
 @dataclass
 class Packet:
-    category: str  # VISION, SCREEN
-    event: str     # SLEEPING, GAMING
-    data: dict
+    event: str     # DROWSY, WINDOW_CHANGE...
+    data: Dict[str, Any]
+    meta: PacketMeta
 
+    # 전송용: 객체 -> JSON String 변환
     def to_json(self):
-        return json.dumps(asdict(self))
+        return json.dumps({
+            "meta": asdict(self.meta),
+            "event": self.event,
+            "data": self.data
+        })
 
+    # 수신용: JSON String -> 객체 변환
     @staticmethod
     def from_json(json_str):
         d = json.loads(json_str)
-        return Packet(**d)
+        return Packet(
+            event=d['event'],
+            data=d['data'],
+            meta=PacketMeta(**d['meta'])
+        )
